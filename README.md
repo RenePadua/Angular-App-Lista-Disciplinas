d73456655c05a94f7abf89f0d96f032fc39ebfe0
RenePadua <rene.padua@gmail.com>
2022-03-11 23:29:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-wu81sf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-wu81sf)
---
+++ src/app/list.ts
@@ -0,0 +1,13 @@
+export const listData = [
+
+{id: 1, code: 'ISW014', text: 'Projeto de Encontrabilidade', day: 'Segunda', time: '19:00-22:30'},
+{id: 2, code: 'ACI002', text: 'Criação de Empresas para Internet', day: 'Terça', time: '19:00-20:40'},
+{id: 3, code: 'ING012', text: 'Inglês VI', day: 'Terça', time: '20:50-22:30'},
+{id: 4, code: 'ISD002', text: 'Arquitetura Orientada a Serviços', day: 'Quarta', time: '19:00-22:30'},
+{id: 5, code: 'ITE005', text: 'Tópicos Especiais em Sistemas para Internet III', day: 'Quinta', time: '19:00-22:30'},
+{id: 6, code: 'ACI002', text: 'Criação de Empresas para Internet', day: 'Sexta', time: '19:00-20:40'},
+{id: 7, code: 'ITE004', text: 'Tópicos Especiais em Sistemas para Internet II', day: 'Sexta', time: '19:00-22:30'},
+{id: 8, code: 'ISW013', text: 'Desenvolvimento para Dispositivos Móveis II', day: 'Sábado', time: '13:30-17:00'},
+
+
+]
